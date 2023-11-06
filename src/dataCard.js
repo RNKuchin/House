cf46b47dc09ab5c7475b669ab69c1cd3a38333fb
Roman Kuchin <roman.kuchin@mail.com>
2023-11-06 19:26:16 +0500

    +

--- src/dataCard.js
+++ src/dataCard.js
@@ -5,7 +5,7 @@ const dataCard = [
     title: 'Охота Амура',
     material: 'Холст, масло (50х80) ',
     price: '14 500 руб',
-    urlImg: 'src/assets/artCard.jpg',
+    urlImg: 'images/artCard.jpg',
   },
   {
     id: '2',
@@ -13,7 +13,7 @@ const dataCard = [
     title: 'Дама с собачкой',
     material: 'Акрил, бумага (50х80) ',
     price: '16 500 руб',
-    urlImg: 'src/assets/Lady_with_a_dog.jpg',
+    urlImg: 'images/Lady_with_a_dog.jpg',
   },
   {
     id: '3',
@@ -21,7 +21,7 @@ const dataCard = [
     title: 'Процедура',
     material: 'Цветная литография (40х60) ',
     price: '20 000 руб',
-    urlImg: 'src/assets/Procedure.jpg',
+    urlImg: 'images/Procedure.jpg',
   },
   {
     id: '4',
@@ -29,7 +29,7 @@ const dataCard = [
     title: 'Роза',
     material: 'Бумага, акрил (50х80) ',
     price: '12 000 руб',
-    urlImg: 'src/assets/Rose.jpg',
+    urlImg: 'images/Rose.jpg',
   },
   {
     id: '5',
@@ -37,7 +37,7 @@ const dataCard = [
     title: 'Птичья трапеза',
     material: 'Цветная литография (40х60) ',
     price: '22 500 руб',
-    urlImg: 'src/assets/Bird_is_meal.jpg',
+    urlImg: 'images/Bird_is_meal.jpg',
   },
   {
     id: '6',
@@ -45,7 +45,7 @@ const dataCard = [
     title: 'Пейзаж с рыбой',
     material: 'Цветная литография (40х60) ',
     price: '20 000 руб',
-    urlImg: 'src/assets/Landscape_with_fish.jpg',
+    urlImg: 'images/Landscape_with_fish.jpg',
   },
 ];
 
